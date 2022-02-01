83a8c4e47cc3d713caa3e963e8e2de4e077c5acb
vuhk <vu.ho@onemount.com>
2022-02-02 00:34:48 +0700

    v2

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <script setup>
-import Intro from "./pages/Intro.vue";
-import AboutMe from "./pages/AboutMe.vue";
-import Index from "./pages/Index.vue"
 </script>
 
 <template>
-<Index/>
 </template>
 
 <style>
-@font-face {
-  font-family: "Instagram";
-  src: url("./assets/fonts/instagram.otf");
-}
 
 </style>
