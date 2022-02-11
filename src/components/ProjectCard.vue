1a7b4ed411dc24a4d9ea8af8cf3af7f733bb9008
vu.ho <qwerty22121998@gmail.com>
2022-02-11 22:54:11 +0700

    project card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -2,23 +2,25 @@
   <div
     class="flex flex-col border p-5 shadow h-full justify-between bg-slate-100"
   >
-    <div class="text-2xl bg-black text-white">{{ name }}</div>
-    <div v-if="tags">
+    <div class="pl-2 py-3 text-2xl bg-black text-white">{{ name }}</div>
+    <div v-if="tags" class="text-slate-500 text-xs">
       <span v-for="tag in tags" :key="tag"> #{{ tag }} </span>
     </div>
     <hr />
-    <div class="text-xl">{{ short }}</div>
+    <div class="text-base">{{ short }}</div>
     <img :src="img" />
-    <div class="flex space-x-5 justify-between">
+    <div class="flex space-x-5 pt-5 justify-between">
       <div v-if="source">
         <a :href="source" class="underline">
-          <i class="fa-solid fa-code"></i>
-          Source code
+          <i class="fa-brands fa-github" /> Source code
         </a>
       </div>
       <div v-if="live">
         <a :href="live" class="underline">
-          <i class="fa-solid fa-satellite-dish" /> Live demo
+          <i class="fa-solid fa-satellite-dish relative">
+            <i class="fa-satellite-dish absolute top-0 left-0 animate-ping" />
+          </i>
+          Live demo
         </a>
       </div>
     </div>
